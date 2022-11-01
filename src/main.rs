34849c351a467573bdf06a3e1356f3ec5848ab85
wizardpisces <1275839779@qq.com>
2022-11-01 16:32:01 +0800

    feat:init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+mod lang_item;
+mod console;
+mod sbi;
+
+
+use core::arch::global_asm;
+use crate::sbi::shutdown;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
